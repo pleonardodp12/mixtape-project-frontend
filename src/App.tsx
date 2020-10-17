3a9e27b82468261b58f640bd7906eacd1cc63b63
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-10-17 18:08:19 -0300

    tests: added test configuration files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import TapeButton from './components/TapeButton/TapeButton';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <TapeButton type='play'/>
     </div>
   );
 }
