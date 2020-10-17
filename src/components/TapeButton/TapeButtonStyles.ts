014fc82e86f16777f4c2a46b69348569d70d881a
Mariana Mattos <marianasmmattos@gmail.com>
2020-10-17 18:41:46 -0300

    add css root

--- src/components/TapeButton/TapeButtonStyles.ts
+++ src/components/TapeButton/TapeButtonStyles.ts
@@ -1,10 +1,5 @@
 import styled from 'styled-components';
 
-const primaryColor = '#3b3030';
-const secondaryColor = '#f07c7c';
-const inactiveIcon = '#655F5F';
-const inactiveButton = '#242424';
-
 export const StyledButton = styled.button`
   margin: 5%;
   border-radius: 5px;
@@ -13,11 +8,12 @@ export const StyledButton = styled.button`
   width: 50%;
 
   &.active {
-    background-color: ${primaryColor};
-    color: ${secondaryColor};
+    background-color: var(--primaryColor);
+    color: var(--secondaryColor);
   }
+  
   &.inactive {
-    background-color: ${inactiveButton};
-    color: ${inactiveIcon};
+    background-color: var(--inactiveButton);
+    color: var(--inactiveIcon);
   }
 `;
